b12786b1518b5af44232b13c97c070952ed7aabd
yacin235 <yacinnyassine@gmail.com>
2019-10-15 00:34:35 +0100

    added what i wanted

--- express/server.js
+++ express/server.js
@@ -12,6 +12,46 @@ router.get('/', (req, res) => {
     })
 });
 
+// Facebook
+router.get('/webhook/', (req, res) =>{
+  if (req.query['hub.verify_token'] === "yacin235") {
+      res.send(req.query['hub.challenge'])
+  }
+  res.send('Wrong token');
+});
+//  Send 
+router.post('/webhook/', (req, res) =>{
+  let messaging_events = req.body.entry[0].messaging;
+  for (let i = 0 ; i < messaging_events.length; i++){
+      let event = messaging_events[i]
+      let sender = event.sender.id;
+      if (event.message && event.message.text){
+          let text = event.message.text;
+          sendText(sender, "Text echo" + text.substring(0,100));
+      }
+  }
+});
+
+function sendText(sender, text){
+  let messageData = {text: text};
+  request({
+      url : "https://graph.facebook.com/v2.6/me/messages",
+      qs: {access_token: token},
+      method: "POST",
+      json: {
+          recipient : {id: sender},
+          message: messageData
+      }
+  }, (error, response, body)=>{
+      if (error) {
+          console.log("sending error");
+      }else if (response.body.error) {
+          console.log(response.body.error)
+      }
+  })
+}
+
+
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 
